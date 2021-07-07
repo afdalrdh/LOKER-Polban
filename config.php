bdfb77b1a91f7919768957479572b21a1da354a7
afdalrdh <afdalramdan@gmail.com>
2021-07-08 06:28:48 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+	require_once __DIR__ . "/vendor/autoload.php";
+	$user = (new MongoDB\Client)->db_tubes_loker->user;
+	$admin = (new MongoDB\Client)->db_tubes_loker->admin;
+	$loker = (new MongoDB\Client)->db_tubes_loker->loker;
+	$pelamar = (new MongoDB\Client)->db_tubes_loker->pelamar;
